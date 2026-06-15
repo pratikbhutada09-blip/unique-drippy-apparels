e9aadd19e99303f82a3c3337b30e7ac14b805e50
pratikbhutada09-blip <pratikbhutada09@gmail.com>
2026-06-15 17:10:16 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,34 @@
-import Hero from '../components/Hero';
-import Collection from '../components/Collection';
-import Customize from '../components/Customize';
-import About from '../components/About';
-import Contact from '../components/Contact';
+// src/pages/Home.jsx
+
+import Hero from "../components/Hero";
+import Collection from "../components/Collection";
+import Customize from "../components/Customize";
+import About from "../components/About";
+import Stats from "../components/Stats";
+import Testimonials from "../components/Testimonials";
+import Gallery from "../components/Gallery";
+import CTA from "../components/CTA";
+import Contact from "../components/Contact";
 
 const Home = () => {
   return (
     <>
       <Hero />
+
       <Collection />
+
       <Customize />
+
       <About />
+
+      <Stats />
+
+      <Testimonials />
+
+      <Gallery />
+
+      <CTA />
+
       <Contact />
     </>
   );
