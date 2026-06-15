9a30a8e38411bab1971641248e7cdea5e47bfa29
pratikbhutada09-blip <pratikbhutada09@gmail.com>
2026-06-15 18:40:17 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import { Menu, X } from "react-feather";
+import { FiMenu, FiX } from "react-icons/fi";
 
 const Navbar = () => {
   const [scrolled, setScrolled] = useState(false);
