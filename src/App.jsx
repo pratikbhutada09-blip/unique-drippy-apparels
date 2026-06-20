71138e131de15c5eef592c3e397776c88ed3b33c
pratikbhutada09-blip <pratikbhutada09@gmail.com>
2026-06-20 21:28:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -6,33 +6,30 @@ import FloatingWhatsApp from "./components/FloatingWhatsApp";
 import FloatingInstagram from "./components/FloatingInstagram";
 
 import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import Collections from "./pages/Collections";
+import Customize from "./pages/Customize";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <Router>
-      <div className="bg-white text-black min-h-screen overflow-x-hidden">
+    <BrowserRouter>
+      <Navbar />
 
-        <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/collections" element={<Collections />} />
+        <Route path="/customize" element={<Customize />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
 
-        <main>
-          <Routes>
-            <Route
-              path="/"
-              element={<Home />}
-            />
-          </Routes>
-        </main>
-
-        <Footer />
-
-        {/* Floating Instagram Button */}
-        <FloatingInstagram />
-
-        {/* Floating WhatsApp Button */}
-        <FloatingWhatsApp />
-
-      </div>
-    </Router>
+      <Footer />
+      <FloatingWhatsApp />
+      <FloatingInstagram />
+    </BrowserRouter>
   );
 }
 
