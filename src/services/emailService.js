6482449b78f677361c2044a2b6297ae70128132e
pratikbhutada09-blip <pratikbhutada09@gmail.com>
2026-06-20 21:17:46 +0530

    Update emailService.js

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,17 +1,17 @@
 import emailjs from "@emailjs/browser";
 
 export const sendEmail = async (data) => {
-  return emailjs.send(
-    "service_2fo834h",
-    "template_lu07swf",
-    {
-      name: data.name,
-      phone: data.phone,
-      email: data.email,
-      product: data.product,
-      quantity: data.quantity,
-      message: data.message,
-    },
-    "aR7mu514jlnNrVSda"
-  );
+return emailjs.send(
+"service_2fo834h",
+"template_ztsat0u",
+{
+name: data.name,
+phone: data.phone,
+email: data.email,
+product: data.product,
+quantity: data.quantity,
+message: data.message,
+},
+"aR7mu514jlnNrVSda"
+);
 };
