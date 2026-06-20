a0d3d62ddbdc7af4af69eebdcde5ce6666f1b744
pratikbhutada09-blip <pratikbhutada09@gmail.com>
2026-06-20 21:29:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -8,29 +8,63 @@ import FloatingInstagram from "./components/FloatingInstagram";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import Collections from "./pages/Collections";
-import Customize from "./pages/Customize";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
+import CustomizePage from "./pages/Customize";
+import AboutPage from "./pages/About";
+import ContactPage from "./pages/Contact";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
+return ( <Router> <div className="bg-white text-black min-h-screen overflow-x-hidden">
 
+
+    <Navbar />
+
+    <main>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/collections" element={<Collections />} />
-        <Route path="/customize" element={<Customize />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
 
-      <Footer />
-      <FloatingWhatsApp />
-      <FloatingInstagram />
-    </BrowserRouter>
-  );
+        <Route
+          path="/"
+          element={<Home />}
+        />
+
+        <Route
+          path="/shop"
+          element={<Shop />}
+        />
+
+        <Route
+          path="/collections"
+          element={<Collections />}
+        />
+
+        <Route
+          path="/customize"
+          element={<CustomizePage />}
+        />
+
+        <Route
+          path="/about"
+          element={<AboutPage />}
+        />
+
+        <Route
+          path="/contact"
+          element={<ContactPage />}
+        />
+
+      </Routes>
+    </main>
+
+    <Footer />
+
+    <FloatingWhatsApp />
+
+    <FloatingInstagram />
+
+  </div>
+</Router>
+
+
+);
 }
 
 export default App;
